2d550c0d61a543441828f4e15d1b5c775e4433b9
GuilhermeMachado12 <gui.perlin6@gmail.com>
2025-08-28 22:01:03 -0300

    Delete Cadastro_Medico_e_Usuario directory

--- Cadastro_Medico_e_Usuario/Telas/Medico/Medico.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-import { initDB, getUsuarios, addUsuario } from "../../db/db";
-import { View, StyleSheet, Text, TouchableOpacity, FlatList } from 'react-native';
-import Estilos from '../../Componentes/Estilos';
-import TextoInput from "../../Componentes/TextoInput";
-import md5 from 'md5';
-
-export default function Medico(props) {
-}
--- Cadastro_Medico_e_Usuario/Telas/Usuario/Usuario.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput, Alert } from 'react-native';
-import { initDB, addUsuario, getUsuarios, deletarUsuario, alterarUsuario } from '../../db/db';
-import Estilos from '../../Componentes/Estilos';
-import TextoInput from "../../Componentes/TextoInput";
-import md5 from 'md5';
-
-export default function Usuario(props) {
-}
